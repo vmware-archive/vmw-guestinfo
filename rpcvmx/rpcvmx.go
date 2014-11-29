d0f001d139ac669ede3b66eff98a4570de7839a2
Yann Hodique <hodiquey@vmware.com>
2014-11-29 09:41:25 -0800

    add a few high-level helpers

--- rpcvmx/rpcvmx.go
+++ rpcvmx/rpcvmx.go
@@ -1,6 +1,10 @@
 package rpcvmx
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/sigma/vmw-guestinfo/rpcout"
 )
 
@@ -10,4 +14,30 @@ func ConfigGetString(key string, default_value string) string {
 		return default_value
 	}
 	return out
+}
+
+func ConfigGetBool(key string, default_value bool) bool {
+	val := strings.ToLower(ConfigGetString(
+		key, fmt.Sprintf("%t", default_value)))
+	if val == "true" {
+		return true
+	} else if val == "false" {
+		return false
+	} else {
+		return default_value
+	}
+}
+
+func ConfigGetInt(key string, default_value int) int {
+	val := ConfigGetString(key, "")
+
+	if val != "" {
+		res, err := strconv.Atoi(val)
+		if err != nil {
+			return default_value
+		}
+		return res
+	} else {
+		return default_value
+	}
 }
