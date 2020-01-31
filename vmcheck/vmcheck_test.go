b2c10264ff19c426255b5e2517d81e1725850ce4
Yann Hodique <yhodique@google.com>
2020-01-31 12:51:41 -0800

    vmcheck: mock the platform and test multiple scenarios.
    Previously the test would only work on friendly hosts. With this change we test
    more options, and don't rely on the actual host behavior.

--- vmcheck/vmcheck_test.go
+++ vmcheck/vmcheck_test.go
@@ -15,19 +15,80 @@
 package vmcheck
 
 import (
+	"encoding/binary"
+	"fmt"
 	"testing"
-
-	"github.com/vmware/vmw-guestinfo/internal"
 )
 
-func TestIsVirtualWorld(t *testing.T) {
-	isBackdoor, err := defaultPlatform.hypervisorPortCheck()
-	internal.AssertNoError(t, err)
+var (
+	intelID = []byte("GenuineIntel")
+	vmwID   = []byte("VMwareVMware")
 
-	t.Log("Backdoor available: ", isBackdoor)
-	t.Log("CPU HV: ", IsVirtualCPU())
+	errPerm   = fmt.Errorf("operation not permitted")
+	errAccess = fmt.Errorf("access denied")
+)
 
-	isVM, err := IsVirtualWorld()
-	internal.AssertNoError(t, err)
-	t.Log("Running in a VM: ", isVM)
+func TestVirtualWorld(t *testing.T) {
+	leUint32 := binary.LittleEndian.Uint32
+	data := []struct {
+		scenario   string
+		hv         bool
+		id         []byte
+		portAccess error
+		bdoorKnock error
+		resp       bool
+	}{
+		{"vmware", true, vmwID, nil, nil, true},
+		{"vmware no backdoor", true, vmwID, errPerm, errAccess, false},
+		{"physical", false, intelID, errPerm, errAccess, false},
+	}
+
+	for _, tt := range data {
+		tt := tt
+		t.Run(tt.scenario, func(t *testing.T) {
+			p := platform{
+				accessPorts: func() error { return tt.portAccess },
+				cpuid: func(eax, _ uint32) (uint32, uint32, uint32, uint32) {
+					if eax == 0x1 {
+						if tt.hv {
+							return 0, 0, uint32(1 << 31), 0
+						}
+						return 0, 0, 0, 0
+					}
+					if eax == 0x40000000 {
+						return 0, leUint32(tt.id[:]), leUint32(tt.id[4:]), leUint32(tt.id[8:])
+					}
+					t.Fatal(fmt.Errorf("unexpected cpuid call"))
+					return 0, 0, 0, 0
+				},
+				knock: func() (bool, error) {
+					if tt.bdoorKnock == nil {
+						return true, nil
+					}
+					return false, tt.bdoorKnock
+				},
+			}
+			v, err := p.isVirtualWorld()
+			if tt.resp {
+				if !v || err != nil {
+					t.Fatal("expected to be virtual")
+				}
+				return
+			}
+
+			if v {
+				t.Fatal("wrongly detected as virtual")
+			}
+
+			if err != nil {
+				if tt.portAccess != nil && err == tt.portAccess {
+					return
+				}
+				if tt.bdoorKnock != nil && err == tt.bdoorKnock {
+					return
+				}
+				t.Fatal("unexpected error")
+			}
+		})
+	}
 }
